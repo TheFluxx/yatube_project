85e45b6fd91db8d1eff5f26a0f48c4a5fe44da9a
Платон Жемеров <zhemerov.p@mail.ru>
2022-05-25 16:52:43 +0300

    Добавлены urls и view-функция

---
+++ yatube/groups/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.index),
+    path('group/', views.group_posts),
+    path('group/<slug:pk>/', views.group_number),
+]
+
+
---
+++ yatube/groups/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse('Главная страница')
+
+def group_posts(request):
+    return HttpResponse('Посты, отфильтрованные по группам')
+
+def group_number(request, pk):
+    return HttpResponse(f'Пост номер {pk}')
