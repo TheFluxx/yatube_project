838b7551e9f9248de877d3f1c1b4f58dfe229738
Платон <zhemerov17@mail.ru>
2022-07-28 21:58:34 +0300

    last

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = 'posts'
 
 urlpatterns = [
-    path('', views.index, name='main_page'),
+    path('', views.index, name='posts'),
     path('group_list.html/', views.group_posts, name='group_list'),
 ]
 
